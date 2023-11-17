7498f0048e6a3c007d9290323f6218bb30537718
Fantik94 <ringlerbaptiste@gmail.com>
2023-11-17 16:34:27 +0100

    push

--- src/Controller/ReservationController.php
+++ src/Controller/ReservationController.php
@@ -10,28 +10,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class ReservationController extends AbstractController
 {
     #[Route('/reservation/{id}', name: 'app_reservation')]
-    public function index(VehiculeRepository $vehiculeRepository, int $id, Request $request): Response
+    public function index(VehiculeRepository $vehiculeRepository, int $id, Request $request, SessionInterface $session): Response
     {
         if (!$this->getUser()) {
+            $session->set('redirect_url', $request->getRequestUri());
             return $this->redirectToRoute('app_login');
         }
-    
+
         $vehicule = $vehiculeRepository->find($id);
         if (!$vehicule) {
             throw $this->createNotFoundException('Véhicule non trouvé.');
         }
-    
+
         return $this->render('reservation/index.html.twig', [
             'vehicle' => $vehicule,
             'start_date' => $request->query->get('start_date'),
             'end_date' => $request->query->get('end_date')
         ]);
     }
-    
+
     #[Route('/process_reservation/{id}', name: 'app_process_reservation', methods: ['POST'])]
     public function processReservation(Request $request, VehiculeRepository $vehiculeRepository, EntityManagerInterface $entityManager, int $id): Response
     {
@@ -39,28 +41,28 @@ class ReservationController extends AbstractController
         if (!$user instanceof Membre) {
             throw new \LogicException('Utilisateur non autorisé.');
         }
-    
+
         $vehicule = $vehiculeRepository->find($id);
         if (!$vehicule) {
             throw $this->createNotFoundException('Véhicule non trouvé.');
         }
-    
+
         $dateDebut = new \DateTime($request->request->get('start_date'));
         $dateFin = new \DateTime($request->request->get('end_date'));
         $interval = $dateDebut->diff($dateFin);
         $jours = $interval->days;
         $prixTotal = $jours * $vehicule->getPrixJournalier();
-    
+
         $commande = new Commande();
         $commande->setIdMembre($user->getId());
         $commande->setIdVehicule($vehicule->getIdVehicule());
         $commande->setDateHeureDepart($dateDebut);
         $commande->setDateHeureFin($dateFin);
         $commande->setPrixTotal($prixTotal);
-    
+
         $entityManager->persist($commande);
         $entityManager->flush();
-    
+
         return $this->redirectToRoute('confirmation_page');
     }
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,17 +5,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, SessionInterface $session): Response
     {
-
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        // Après la connexion, vérifiez si une URL de redirection est stockée
+        if ($this->getUser()) {
+            $redirectUrl = $session->get('redirect_url', $this->generateUrl('default_route'));
+            $session->remove('redirect_url'); // Supprimez l'URL de la session
+
+            return $this->redirect($redirectUrl);
+        }
 
         return $this->render('login/index.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
