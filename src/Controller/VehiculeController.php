813f05983a7a559798accb0faf5bcd5f37ff4d4f
Fantik94 <ringlerbaptiste@gmail.com>
2023-11-23 22:01:57 +0100

    backoffice

--- src/Controller/VehiculeController.php
+++ src/Controller/VehiculeController.php
@@ -9,11 +9,13 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VehiculeRepository;
+use Symfony\Component\HttpFoundation\Response;
 
 class VehiculeController extends AbstractController
 {
     #[Route('/ajouter', name: 'ajouter_vehicule')]
-    public function ajouter(Request $request, EntityManagerInterface $entityManager)
+    public function ajouter(Request $request, EntityManagerInterface $entityManager): Response
     {
         $vehicule = new Vehicule();
         $form = $this->createForm(AjouterVehiculeType::class, $vehicule);
@@ -24,18 +26,55 @@ class VehiculeController extends AbstractController
             $entityManager->persist($vehicule);
             $entityManager->flush();
 
-            $this->addFlash('success', 'Voiture bien ajoutée dans la bdd.');
-
-            
-            return $this->render('ajouter-vehicule/index.html.twig', [
-                'form' => $form->createView(),
-                'success' => true 
-            ]);
+            $this->addFlash('success', 'Voiture bien ajoutée dans la base de données.');
+            return $this->redirectToRoute('gestion_vehicules');
         }
 
         return $this->render('ajouter-vehicule/index.html.twig', [
             'form' => $form->createView(),
-            'success' => false 
         ]);
+    }
+
+    #[Route('/gestion', name: 'gestion_vehicules')]
+    public function gestion(VehiculeRepository $vehiculeRepository): Response
+    {
+        $vehicules = $vehiculeRepository->findAll();
+
+        return $this->render('vehicule/gestion.html.twig', [
+            'vehicules' => $vehicules,
+        ]);
+    }
+
+    #[Route('/modifier/{id}', name: 'modifier_vehicule')]
+    public function modifier(Request $request, Vehicule $vehicule, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(AjouterVehiculeType::class, $vehicule);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            $this->addFlash('success', 'Véhicule mis à jour avec succès.');
+            return $this->redirectToRoute('gestion_vehicules');
+        }
+
+        return $this->render('vehicule/modifier.html.twig', [
+            'form' => $form->createView(),
+            'vehicule' => $vehicule
+        ]);
+    }
+
+    #[Route('/supprimer/{id}', name: 'supprimer_vehicule')]
+    public function supprimer(Vehicule $vehicule, EntityManagerInterface $entityManager): Response
+    {
+        if (!$vehicule) {
+            $this->addFlash('error', 'Véhicule introuvable.');
+            return $this->redirectToRoute('gestion_vehicules');
+        }
+
+        $entityManager->remove($vehicule);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Le véhicule a été supprimé avec succès.');
+        return $this->redirectToRoute('gestion_vehicules');
     }
 }
